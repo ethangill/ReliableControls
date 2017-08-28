eab01f82b7b0f25cbf9ddab84cad3eb84db3e777
Ethan GIll <Ethan GIll>
2017-08-27 23:48:16 -0700

    Initial Commit Added State Machine Project

---
+++ StateMachineLogger/StateMachineLogger/Enums/Enums.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateMachineLogger.Enums
+{
+	public enum State
+	{
+		Stage0,
+		Stage1,
+		Stage2,
+		Stage3
+	}
+
+	public enum Command
+	{
+		Start,
+		Cycle
+	}
+}
